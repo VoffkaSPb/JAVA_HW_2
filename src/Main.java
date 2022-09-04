11ac1e66f7fb10d8b6a6abdbcc2bb7e3cee4413b
Vladimir <vov.subbotin2015@yandex.ru>
2022-09-04 16:24:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 10000;
+        int rubMiles = 20;
+
+        int BonusMiles = ticketCost / rubMiles;
+
+        System.out.println("Вам начислено: " + BonusMiles + " миль");
+    }
+}
